47a7c14d652db3b1e688384f4995015b2900323c
Maik Schneider <m.schneider@blueways.de>
2021-08-23 07:05:42 +0200

    feat: cache configuration

--- Classes/Utility/HelperUtility.php
+++ Classes/Utility/HelperUtility.php
@@ -3,7 +3,6 @@
 namespace Blueways\BwIcons\Utility;
 
 use Blueways\BwIcons\Provider\CssIconProvider;
-use Blueways\BwIcons\Provider\FileIconProvider;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\TypoScript\TypoScriptService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -17,14 +16,12 @@ class HelperUtility
         $tabs = [];
 
         foreach ($extensionSettings as $key => $providerSettings) {
-            $options = $providerSettings;
-            unset($options['_typoScriptNodeValue']);
-            $provider = GeneralUtility::makeInstance($providerSettings['_typoScriptNodeValue'], $options);
 
             $tab = [];
             $tab['id'] = $key;
             $tab['title'] = $providerSettings['title'];
-            $tab['folders'] = $provider->getIcons();
+            $tab['folders'] = $this->getModalConfFromSettings($key, $providerSettings);
+
             $tabs[] = $tab;
         }
 
@@ -39,6 +36,26 @@ class HelperUtility
         /** @var TypoScriptService $typoscriptService */
         $typoscriptService = GeneralUtility::makeInstance(TypoScriptService::class);
         return $typoscriptService->convertTypoScriptArrayToPlainArray($pageTsConfig['mod.']['tx_bwicons.'] ?? []);
+    }
+
+    protected function getModalConfFromSettings($key, $providerSettings)
+    {
+        $cacheIdentifier = $key;
+        $cache = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Cache\CacheManager::class)->getCache('bwicons_conf');
+
+        if (($value = $cache->get($cacheIdentifier)) === false) {
+
+            // construct Provider and get conf
+            $options = $providerSettings;
+            unset($options['_typoScriptNodeValue']);
+            $provider = GeneralUtility::makeInstance($providerSettings['_typoScriptNodeValue'], $options);
+            $value = $provider->getIcons();
+
+            // save conf
+            $cache->set($cacheIdentifier, $value, [], 0);
+        }
+
+        return $value;
     }
 
     public function getStyleSheets(): array
@@ -59,16 +76,5 @@ class HelperUtility
             $sheets[] = '../' . $setting['file'];
         }
         return $sheets;
-    }
-
-    public function getTagName(string $providerId): string
-    {
-        return 'i';
-    }
-
-    public function isFileIconProvider(string $providerId): bool
-    {
-        $settings = $this->getSettings();
-        return $settings[$providerId]['_typoScriptNodeValue'] === FileIconProvider::class;
     }
 }
--- ext_localconf.php
+++ ext_localconf.php
@@ -7,3 +7,7 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1629534119] = [
     'priority' => 40,
     'class' => \Blueways\BwIcons\Form\Element\IconSelection::class
 ];
+
+if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['bwicons_conf'])) {
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['bwicons_conf'] = [];
+}
