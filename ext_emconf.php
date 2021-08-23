ea19ca0ea81d65fba2bc3fdd2f7b3a8368c3d963
Maik Schneider <m.schneider@blueways.de>
2021-08-23 11:11:55 +0200

    feat: update readme

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Icon Selector',
-    'description' => 'Form element for icon selection. Generates a browsable gallery of your icon fonts and SVGs - just link your stylesheet or image directory. Works with FontAwesome, Bootstrap, Icomoon.. Optionally adds icon field to pages and tt_content.',
+    'description' => 'Form element for icon selection. Generates a browsable gallery of your icon fonts and SVGs - just link your stylesheet or image directory that you use in the frontend! Works with FontAwesome, Bootstrap, Icomoon.. Optionally adds icon field to pages and tt_content.',
     'category' => 'be',
     'author' => 'Maik Schneider',
     'author_email' => 'm.schneider@blueways.de',
