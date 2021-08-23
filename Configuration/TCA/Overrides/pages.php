4b84557b100c67b36a15dd1c4279c90890dbc709
Maik Schneider <m.schneider@blueways.de>
2021-08-23 08:18:06 +0200

    feat: add icon field to pages

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -7,3 +7,17 @@ defined('TYPO3_MODE') or die();
     'Configuration/TSconfig/Page/Typo3Icons.tsconfig',
     'TYPO3 Core Icons'
 );
+
+$GLOBALS['TCA']['pages']['columns']['tx_bwicons_icon'] = [
+    'exclude' => 1,
+    'label' => 'Icon',
+    'config' => [
+        'type' => 'input',
+        'renderType' => 'iconSelection'
+    ],
+];
+
+$firstBreak = strpos($GLOBALS['TCA']['pages']['palettes']['title']['showitem'], '--linebreak--');
+$GLOBALS['TCA']['pages']['palettes']['title']['showitem'] = substr_replace($GLOBALS['TCA']['pages']['palettes']['title']['showitem'],
+    'tx_bwicons_icon,',
+    $firstBreak, 0);
