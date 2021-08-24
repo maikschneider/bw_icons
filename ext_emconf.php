dae8f315ce5933680ddce3660ce7d6eb51e0b320
Maik Schneider <m.schneider@blueways.de>
2021-08-24 11:22:04 +0200

    feat: rename extension to "Icon Picker"

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,6 +1,6 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Icon Selector',
+    'title' => 'Icon Picker',
     'description' => 'Form element for icon selection. Generates a browsable gallery of your icon fonts and SVGs - just link your stylesheet or image directory that you use in the frontend! Works with FontAwesome, Bootstrap, Icomoon.. Optionally adds icon field to pages and tt_content.',
     'category' => 'be',
     'author' => 'Maik Schneider',
