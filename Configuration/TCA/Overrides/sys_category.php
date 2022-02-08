5119afab2607800121a1864549270dec42a0eb4a
Maik Schneider <m.schneider@blueways.de>
2022-02-08 12:09:57 +0100

    feat: add icon field for sys_category

---
+++ Configuration/TCA/Overrides/sys_category.php
@@ -0,0 +1,38 @@
+<?php
+
+defined('TYPO3_MODE') or die();
+
+// Get extension configuration
+$extensionConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+    \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
+);
+$bwiconsConf = $extensionConfiguration->get('bw_icons');
+
+// Add new field if enabled
+if ((int)$bwiconsConf['sys_category'] === 1) {
+    // Create new field
+    $temporaryColumns = [
+        'tx_bwicons_icon' => [
+            'exclude' => 0,
+            'label' => 'LLL:EXT:bw_icons/Resources/Private/Language/locallang.xlf:icon',
+            'config' => [
+                'type' => 'input',
+                'renderType' => 'iconSelection'
+            ],
+        ]
+    ];
+
+    // Register new field
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(
+        'sys_category',
+        $temporaryColumns
+    );
+
+    // Display after title
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+        'sys_category',
+        'tx_bwicons_icon',
+        '',
+        'after:title'
+    );
+}
