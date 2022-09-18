dbf0555b3a074278884d5939bd3490e57b45162a
Maik Schneider <mas@xima.de>
2022-09-18 17:08:28 +0200

    feat: update ext icon, readme, raise version

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => 0,
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.4.0',
+    'version' => '2.0.0',
     'autoload' => [
         'psr-4' => ['Blueways\\BwIcons\\' => 'Classes']
     ],
