d9d1e3f5d4200bd84c41c028583fe6d137a7d440
Maik Schneider <mas@xima.de>
2023-06-22 10:11:57 +0200

    chore: raise version

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => 0,
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '2.1.2',
+    'version' => '2.1.3',
     'autoload' => [
         'psr-4' => ['Blueways\\BwIcons\\' => 'Classes'],
     ],
