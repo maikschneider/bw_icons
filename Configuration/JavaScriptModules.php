5cc103dd1d3f6a5644bada9b9bbc3d1a902583b0
Maik Schneider <mas@xima.de>
2023-09-18 14:37:26 +0200

    fix: add 'backend.form' tag in JavaScriptModule configuration

--- Configuration/JavaScriptModules.php
+++ Configuration/JavaScriptModules.php
@@ -5,6 +5,9 @@ return [
         'core',
         'backend',
     ],
+    'tags' => [
+        'backend.form',
+    ],
     'imports' => [
         '@blueways/bw-icons/' => 'EXT:bw_icons/Resources/Public/ECMAScript6/',
     ],
