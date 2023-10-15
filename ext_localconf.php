7d8b2e479062b1b95eb09ee3112b13ba7cd2437e
Maik Schneider <mas@xima.de>
2023-10-15 23:28:28 +0200

    feat: code style

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,11 +1,11 @@
 <?php
 
+// register custom form element
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1629534119] = [
     'nodeName' => 'iconSelection',
     'priority' => 40,
     'class' => \Blueways\BwIcons\Form\Element\IconSelection::class,
 ];
 
-if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['bwicons_conf']) || !is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['bwicons_conf'])) {
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['bwicons_conf'] = [];
-}
+// register cache
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['bwicons_conf'] ??= [];
