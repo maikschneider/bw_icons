c68b52cb3281d76b92b9762dfcc9d582d8cd79c2
Maik Schneider <maik.schneider@xima.de>
2026-02-16 11:43:48 +0100

    feat: add test for language synchronization

--- Tests/Fixtures/pages.sql
+++ Tests/Fixtures/pages.sql
@@ -1,3 +1,4 @@
 insert into `pages` (`uid`, `pid`, `title`, `slug`, `sys_language_uid`, `l10n_parent`, `l10n_source`, `perms_userid`,
 										 `perms_groupid`, `perms_user`, `perms_group`, `perms_everybody`, `doktype`, `is_siteroot`, `TSconfig`)
-values (1, 0, 'Main', '/', 0, 0, 0, 1, 1, 31, 31, 1, 1, 1, '');
+values (1, 0, 'Main', '/', 0, 0, 0, 1, 1, 31, 31, 1, 1, 1, ''),
+       (2, 0, 'Hauptseite', '/de/', 1, 1, 1, 1, 1, 31, 31, 1, 1, 0, '');
